8c8e0d5102155e9e8284ba9f0d1e5747cfbf08df
Kevin Effertz <kevin.effertz@gmail.com>
2020-06-27 09:11:37 -0500

    Script files located in gatsby-browser are not being found for github pages when the --prefix-path is used.  Setting assetPrefix should resolve this.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   pathPrefix: "/portedPersonalPage1",
+  assetPrefix: "/portedPersonalPage1",
   plugins: [
     {
       resolve: 'gatsby-plugin-web-font-loader',
